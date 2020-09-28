0dc7aecb528e54573c80e37b86e6fc5cb2446e44
Asim Iqbal <asimiqbalsiddiqui.1@gmail.com>
2020-09-28 16:18:29 +0500

    updated admin routes for frontend

--- modules/admin/controller/adminController.js
+++ modules/admin/controller/adminController.js
@@ -5,25 +5,28 @@ exports.getAllGroups = async (req, res) => {
     try {
         const ITEMS_PER_PAGE = 10;
         const currentPage = req.params.pageNum - 1;
+        GroupsModel.countDocuments(async function (err, c) {
+            let groups = await GroupsModel
+                .find({})
+                .skip(ITEMS_PER_PAGE * currentPage)
+                .limit(ITEMS_PER_PAGE)
+                .populate("members")
+                .lean();
 
-        let groups = await GroupsModel
-            .find({})
-            .skip(ITEMS_PER_PAGE * currentPage)
-            .limit(ITEMS_PER_PAGE)
-            .populate("members")
-            .lean();
+            if (groups) {
+                return res.status(200).json({
+                    groups: groups,
+                    count: groups.length,
+                    total_groups: c
+                })
+            }
+            else {
+                return res.send({
+                    error: "Something goes wrong"
+                })
+            }
+        });
 
-        if (groups) {
-            return res.status(200).json({
-                groups: groups,
-                count: groups.length
-            })
-        }
-        else {
-            return res.send({
-                error: "Something goes wrong"
-            })
-        }
     } catch (err) {
         return res.json({ error: err })
     }
@@ -33,24 +36,28 @@ exports.getAllUsers = async (req, res) => {
     try {
         const ITEMS_PER_PAGE = 30;
         const currentPage = req.params.pageNum - 1;
+        UsersModel.countDocuments(async function (err, c) {
 
-        let users = await UsersModel
-            .find({})
-            .skip(ITEMS_PER_PAGE * currentPage)
-            .limit(ITEMS_PER_PAGE)
-            .lean();
+            let users = await UsersModel
+                .find({})
+                .skip(ITEMS_PER_PAGE * currentPage)
+                .limit(ITEMS_PER_PAGE)
+                .lean();
 
-        if (users) {
-            return res.status(200).json({
-                users: users,
-                count: users.length
-            })
-        }
-        else {
-            return res.send({
-                error: "Something goes wrong"
-            })
-        }
+            if (users) {
+                return res.status(200).json({
+                    users: users,
+                    count: users.length,
+                    total_users: c
+                })
+            }
+            else {
+                return res.send({
+                    error: "Something goes wrong"
+                })
+            }
+
+        });
     } catch (err) {
         return res.json({ error: err })
     }
