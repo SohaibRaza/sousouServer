08224e341bfe22773f458ac21bd89ec0e640e114
tamjeedhur <tamjeedhur@gmail.com>
2020-10-01 03:42:12 +0500

    with user and admin rights

--- modules/admin/routes/admin.js
+++ modules/admin/routes/admin.js
@@ -4,10 +4,10 @@ const controller = require('../controller/adminController');
 const authorize = require('../../../utils/authorize');
 
 // GET ALL GROUPS
-router.get('/groups/:pageNum', authorize, controller.getAllGroups);
+router.post('/groups/:pageNum', authorize, controller.getAllGroups);
 
 // GET ALL USERS
-router.get('/users/:pageNum', authorize, controller.getAllUsers);
+router.post('/users/:pageNum', authorize, controller.getAllUsers);
 
 
 module.exports = router;
